81ae08c56c41431e868cb2a288e83592cb750eca
Maxmillian <maxmillianafanga@gmail.com>
2024-03-20 14:38:38 -0400

    Add user role

--- InventoryManagementApp/server/models/UserModel.js
+++ InventoryManagementApp/server/models/UserModel.js
@@ -25,7 +25,12 @@ const UserSchema = new mongoose.Schema({
       type: String,
       required: [true, 'Password is required']
     },
-    salt: String
+    salt: String,
+    role: {
+      type: String,
+      enum: ['user', 'admin'],
+      default: 'user'
+  }
   });
   
   module.exports = mongoose.model('User', UserSchema);
