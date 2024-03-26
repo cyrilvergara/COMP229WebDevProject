a22ee0ac6d40978e2e3986c31be80f920a25f739
DM Brar <dmvbrar@gmail.com>
2024-03-25 21:13:13 -0400

    set up theme

--- InventoryManagementApp/client/src/App.jsx
+++ InventoryManagementApp/client/src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/styles';
 import MainRouter from '../MainRouter';
 import theme from '../theme';
-//import { hot } from 'react-hot-loader'
 const App = () => {
   return (
     <Router>
