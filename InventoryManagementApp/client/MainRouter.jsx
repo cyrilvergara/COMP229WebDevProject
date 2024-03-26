bb935f8de304bbad978215783c4f1246dda41822
DM Brar <dmvbrar@gmail.com>
2024-03-25 21:30:53 -0400

    create /test for testing component styles

--- InventoryManagementApp/client/MainRouter.jsx
+++ InventoryManagementApp/client/MainRouter.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './core/Home'
+import Test from './core/Test'
 const MainRouter = () => {
     return (<div>
         <Routes>
             <Route exact path="/" element={<Home />} />
+            <Route exact path="/test" element={<Test />} />
         </Routes>
     </div>
     )
---
+++ InventoryManagementApp/client/core/Test.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import wdLogo from './../assets/images/WinterDevLogo_PrimaryLogoLight.svg';
+
+const useStyles = makeStyles(theme => ({
+    // card: {
+    //     maxWidth: 600,
+    //     margin: 'auto',
+    //     marginTop: theme.spacing(5),
+    // },
+    // title: {
+    //     padding: theme.spacing(3, 2.5, 2),
+    //     color: theme.palette.openTitle,
+    // },
+    // media: {
+    //     minHeight: 400,
+    // },
+}));
+export default function Test() {
+    const classes = useStyles()
+    return (
+        <Card className={classes.card}>
+
+            <Typography variant="h6" className={classes.title}>Test Page</Typography>
+            <Typography variant="h6" className={classes.title}>This page is for testing the component styles</Typography>
+            <CardContent>
+                <Typography variant="body2" component="p">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </Typography>
+            </CardContent>
+        </Card>
+    )
+}
+
