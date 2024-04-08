424f771d857362ddaee9835cd17cc9921ccce6bc
DM Brar <dmvbrar@gmail.com>
2024-04-08 15:03:31 -0400

    added cssbaseline to app.jsx to remove unnecessary spacing

--- InventoryManagementApp/client/src/App.jsx
+++ InventoryManagementApp/client/src/App.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import MainRouter from '../MainRouter';
 import { ThemeProvider } from '@material-ui/styles';
 import theme from '../theme';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 const App = () => {
   return (
     <Router>
       <ThemeProvider theme={theme}>
         <MainRouter />
+        <CssBaseline />
       </ThemeProvider>
     </Router>
   );
