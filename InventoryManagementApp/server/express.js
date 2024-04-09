84a2290366d32691a07c6d36e5d56f04b2f80476
JC Borlagdan <>
2024-04-09 14:24:53 -0400

    working on local

--- InventoryManagementApp/server/express.js
+++ InventoryManagementApp/server/express.js
@@ -1,42 +1,36 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const path = require("path");
 const userRoutes = require("./routes/UserRoute");
 const authRoutes = require("./routes/AuthRouter");
 const inventoryRoutes = require("./routes/InventoryRouter");
-const multer = require("multer");
-const path = require("path");
+const cors = require("cors");
 
 const app = express();
 const CURRENT_WORKING_DIR = process.cwd();
+const clientDistPath = path.join(CURRENT_WORKING_DIR, 'client', 'dist', 'app');
 
-console.log(CURRENT_WORKING_DIR);
-console.log(path.join(CURRENT_WORKING_DIR, "dist/app"));
-
-app.use("/dist", express.static(path.join(CURRENT_WORKING_DIR, "dist/app")));
+// Middleware for parsing JSON and urlencoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// Middleware to handle file uploads
-//app.use(upload.single('csv')); // 'csv' should match the name attribute in the file input of your form
-
-
-// Mount user routes
-app.use("/api/users", userRoutes);
-
-// Mount Auth routes
-app.use("/api/auth", authRoutes);
-
-// Mount inventory routes
-app.use("/api/inventory", inventoryRoutes);
-
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(cookieParser());
-// app.use(compress());
-// app.use(helmet());
 app.use(cors());
 
+// Mount API routes
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/inventory", inventoryRoutes);
+
+// Serve static files from the React app build directory
+app.use(express.static(clientDistPath));
+
+// Handles any requests that don't match the ones above
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(clientDistPath, 'index.html'));
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
 module.exports = app;
