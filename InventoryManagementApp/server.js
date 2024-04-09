d90325a00149bdd020fd28cb86327df991b54525
cyrilvergara <cyrilvergara@gmail.com>
2024-04-09 17:14:58 -0400

    push lang

--- InventoryManagementApp/server.js
+++ InventoryManagementApp/server.js
@@ -1,22 +1,21 @@
-const dotenv = require('dotenv');
-const config = require('./server/config/config.js');
-const app = require('./server/express.js');
-const mongoose = require('mongoose');
+const config = require("./server/config/config.js");
+const app = require("./server/express.js");
+const mongoose = require("mongoose");
 
-const fs = require('fs');
+const fs = require("fs");
 
-const uploadDir = './uploads';
-
+const uploadDir = "./uploads";
 
 mongoose.Promise = global.Promise;
 
 // Connect to MongoDB database
-mongoose.connect(config.mongoUri)
+mongoose
+  .connect(config.mongoUri)
   .then(() => {
     console.log("Connected to the database!");
     // Start the Express server after successful database connection
     app.listen(config.port, () => {
-      console.info('Server started on port %s.', config.port);
+      console.info("Server started on port %s.", config.port);
     });
   })
   .catch((err) => {
@@ -24,20 +23,15 @@ mongoose.connect(config.mongoUri)
     process.exit(1); // Exit the application if unable to connect to the database
   });
 
-
-
 // Check if the directory exists, if not create it
 if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir);
-    console.log('Upload directory created');
+  fs.mkdirSync(uploadDir);
+  console.log("Upload directory created");
 } else {
-    console.log('Upload directory already exists');
+  console.log("Upload directory already exists");
 }
 
-
-
-
 // Handle MongoDB connection errors
-mongoose.connection.on('error', (err) => {
+mongoose.connection.on("error", (err) => {
   console.error("MongoDB connection error:", err);
 });
