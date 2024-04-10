b025fbf169b9a80d0290e903786ca3edfad39adf
DM Brar <dmvbrar@gmail.com>
2024-04-09 21:52:28 -0400

    finished create new item

--- InventoryManagementApp/client/src/components/inventory/AddItem.jsx
+++ InventoryManagementApp/client/src/components/inventory/AddItem.jsx
@@ -28,11 +28,27 @@ const useStyles = makeStyles((theme) => ({
     borderRadius: '8px',
     padding: '24px',
   },
+  cardTitle: {
+    fontWeight: 700,
+  },
+  cardSubtitle: {
+    color: theme.palette.grey[600],
+    marginBottom: '36px',
+  },
   btnPrimary: {
-    width: 'min-content',
     backgroundColor: theme.palette.primary.main,
     color: theme.palette.common.white,
     padding: '12px 24px',
+  },
+  grid: {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(2, minmax(0, 1fr))',
+    gridAutoFlow: 'row',
+    gap: '24px',
+    marginBottom: '36px',
+  },
+  gridFull: {
+    gridColumn: '1 / -1',
   },
 }));
 
@@ -97,66 +113,74 @@ export default function AddItem() {
         Create New Item
       </Typography>
       <Card className={classes.card}>
+        <Typography variant="body1" className={classes.cardTitle}>
+          Record Details
+        </Typography>
+        <Typography variant="body2" className={classes.cardSubtitle}>
+          Please fill in all required fields
+        </Typography>
         <form onSubmit={onSubmitClick}>
           <Grid container className={classes.grid}>
-          <TextField
-            label="Item Name"
-            onChange={onTextChange("itemName")}
-            fullWidth
-            margin="normal"
-            variant="standard"
-          />
-          <TextField
-            label="Description"
-            onChange={onTextChange("description")}
-            fullWidth
-            multiline
-            rows={4}
-            margin="normal"
-            variant="standard"
-          />
-          <TextField
-            label="Price"
-            onChange={onTextChange("price")}
-            fullWidth
-            margin="normal"
-            variant="standard"
-          />
-          <TextField
-            label="Initial Quantity"
-            onChange={onTextChange("availableQty")}
-            fullWidth
-            margin="normal"
-            variant="standard"
-          />
-          <TextField
-            label="Category"
-            onChange={onTextChange("category")}
-            fullWidth
-            margin="normal"
-            variant="standard"
-          />
-          <TextField
-            label="Supplier"
-            onChange={onTextChange("supplier")}
-            fullWidth
-            margin="normal"
-            variant="standard"
-          />
-          <TextField
-            label="Size"
-            onChange={onTextChange("size")}
-            fullWidth
-            margin="normal"
-            variant="standard"
-          />
-          <TextField
-            label="Unit"
-            onChange={onTextChange("unit")}
-            fullWidth
-            margin="normal"
-            variant="standard"
-          />
+            <TextField
+              label="Item Name"
+              onChange={onTextChange("itemName")}
+              fullWidth
+              margin="normal"
+              variant="standard"
+              className={classes.gridFull}
+            />
+            <TextField
+              label="Description"
+              onChange={onTextChange("description")}
+              fullWidth
+              multiline
+              rows={4}
+              margin="normal"
+              variant="standard"
+              className={classes.gridFull}
+            />
+            <TextField
+              label="Price"
+              onChange={onTextChange("price")}
+              fullWidth
+              margin="normal"
+              variant="standard"
+            />
+            <TextField
+              label="Initial Quantity"
+              onChange={onTextChange("availableQty")}
+              fullWidth
+              margin="normal"
+              variant="standard"
+            />
+            <TextField
+              label="Category"
+              onChange={onTextChange("category")}
+              fullWidth
+              margin="normal"
+              variant="standard"
+            />
+            <TextField
+              label="Supplier"
+              onChange={onTextChange("supplier")}
+              fullWidth
+              margin="normal"
+              variant="standard"
+            />
+            <TextField
+              label="Size"
+              onChange={onTextChange("size")}
+              fullWidth
+              margin="normal"
+              variant="standard"
+            />
+            <TextField
+              label="Unit"
+              onChange={onTextChange("unit")}
+              fullWidth
+              margin="normal"
+              variant="standard"
+            />
           </Grid>
           <Button
             type="submit"
